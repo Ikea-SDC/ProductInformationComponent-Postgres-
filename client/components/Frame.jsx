649b3ad56df9c45646e471a900c646f3dbc1d65c
Jeff Valle <jeffreyvalle@protonmail.com>
2018-09-08 15:46:47 -0700

    Rendered static body for Product information tab

--- client/components/Frame.jsx
+++ client/components/Frame.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
 import TabFrame from './TabFrame.jsx'
-import Tab from './Tab.jsx';
-
+import ProductInfoBodyFrame from './tabs/product-info/ProductInfoBodyFrame.jsx'
 
 const Container = styled.div`
+  -webkit-font-smoothing: antialiased !important;
   float: left;
+  text-align: left;
+  font-family: Verdana, sans-serif;
   margin-top: 20px;
   padding-bottom: 1.04em;
   overflow: hidden;
@@ -14,12 +16,28 @@ const Container = styled.div`
   width: 1060px;
 `;
 
+const Details = styled.div`
+  display: block;
+  font-size: 0.6em;
+`;
+
+
+
 
 class Frame extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             tabTitles: ['Product information', 'Materials and environment', 'Assembly & documents', 'Package details', 'Ratings'],
+            keyFeatures: ['Is really cool and fun to use.', 'At the same time absolutely trash and horrible to use.', 'Hate the color, too bad.', "Leave a bad review we don't care and won't respond"],
+            goodToKnow: ['Longer pieces of text that go into detail about specific features and what they do so you can learn things.', 'Each one of these needs to be separated and look great wow how cool.', 'Here I am adding more text when I said I wouldnt but I really want this to take up more room so I have to.', 'More text is great we need more text someone get me more text what will we do without more text will we starve oh no quick get the text.','Some more text because I need to fill up space not used by the not included section.', 'Last one I am adding, basically talking to myself this is wonderful I dont feel crazy at all','Everyone loves CSS it is the best thing every lets use more words', 'This one is going to be really long and bland because the rest of them seemed to be about the same lengh. Lets add some periods. Because the rest were run ons anyways.', 'Invest in a grammar book.'],
+            careInstructions: ['A few lines here to tell you how to care for it wow.', 'Should work now that we are here okay cool.'],
+            dimensions: {
+                max: '1,000,000,000 lumens',
+                width: 10,
+                diameter: 23,
+                cordLength: '10 ft',
+                power: '10 w'},
         }
     }
 
@@ -32,6 +50,9 @@ class Frame extends React.Component {
         return (
              <Container>
                 {this.state.tabTitles.map(title => <TabFrame title={title}/>)}
+                <Details>
+                    <ProductInfoBodyFrame keyFeatures={this.state.keyFeatures} goodToKnow={this.state.goodToKnow} careInstructions={this.state.careInstructions} dimensions={this.state.dimensions}/>
+                </Details>
             </Container>
         )
     }
--- client/components/tabs/product-info/ProductInfoBodyFrame.jsx
+++ client/components/tabs/product-info/ProductInfoBodyFrame.jsx
@@ -1,7 +1,154 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ProductInfoBodyFrame = () => {
 
+const ProductInfoLeft = styled.div`
+  column-count: 2;
+  width: 75%;
+  column-gap: 20px;
+  padding: 20px 15px;
+  padding-right: 20px;
+  box-sizing: border-box;
+  display: inline-block;
+  vertical-align: top;
+  margin-bottom: 10px;
+`;
+
+const ProductInfoRight = styled.div`
+  width: 24.5%;
+  padding: 20px;
+  font-size: 11px;
+  line-height: 15px;
+  background-color: #fafafa;
+  box-sizing: border-box;
+  display: inline-block;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+const KeyFeatures = styled.div`
+  display: block;
+  margin-right: 0.208em;
+  margin-left: 6px !important;
+`;
+
+const KeyFeaturesUl = styled.ul`
+  display:block;
+  padding: 0 0 0 15px;
+  margin: 0;
+`;
+
+const KeyFeaturesLi = styled.li`
+  margin-bottom: 8px;
+  font-size: 13px;
+  line-height: 18px;
+  margin-bottom: 10px !important;
+`;
+
+const GoodToKnow = styled.div`
+  margin-bottom: 10px !important;
+  display:block;
+`;
+
+const Headline = styled.div`
+  font-size: 13px;
+  color: #333;
+  margin-bottom: 4px;
+  font-weight: bold;
+`;
+
+const DimensionsHeadline = styled.div`
+  font-size: 11px;
+  color: #333;
+  margin-bottom: 4px;
+  font-weight: bold;
+  line-height: 15px;
+`;
+
+const GoodToKnowItem = styled.div`
+  font-size: 13px;
+  line-height: 18px;
+`;
+
+const ProductDimensions = styled.div`
+  display: block;
+  margin-top: 0 !important;
+`;
+
+const Designer = styled.div`
+  margin-bottom: 10px;
+  margin-top: 20px;
+`;
+
+
+const ProductInfoBodyFrame = (props) => {
+    return <div>
+        <ProductInfoLeft>
+          <KeyFeatures>
+            <KeyFeaturesUl>
+              {props.keyFeatures.map(feature => {
+                return <KeyFeaturesLi>{feature}</KeyFeaturesLi>;
+              })}
+            </KeyFeaturesUl>
+          </KeyFeatures>
+          <GoodToKnow>
+            <Headline>Good to Know</Headline>
+            {props.goodToKnow.map(fact => {
+              return <div>
+                  <GoodToKnowItem>{fact}</GoodToKnowItem>
+                  <br />
+                </div>;
+            })}
+            <Headline>Care Instructions</Headline>
+            {props.careInstructions.map(instruction => {
+              return <div>
+                  <GoodToKnowItem>{instruction}</GoodToKnowItem>
+                  <br />
+                </div>;
+            })}
+          </GoodToKnow>
+        </ProductInfoLeft>
+        <ProductInfoRight>
+          <DimensionsHeadline>Product dimensions</DimensionsHeadline>
+          <ProductDimensions>
+            Max.: {props.dimensions.max}
+            <br />
+            Shade width: {props.dimensions.width}
+            <br />
+            Diameter: {props.dimensions.cordLength}
+            <br />
+            Cord length:
+            {props.dimensions.cordLength}
+            <br />
+            Power: {props.dimensions.power}
+            <br />
+            <br />
+            <br />
+          </ProductDimensions>
+          <ProductDimensions>
+            Max.: {props.dimensions.max}
+            <br />
+            Shade width: {props.dimensions.width}
+            <br />
+            Diameter: {props.dimensions.cordLength}
+            <br />
+            Cord length:
+            {props.dimensions.cordLength}
+            <br />
+            Power: {props.dimensions.power}
+            <br />
+            <br />
+            <br />
+          </ProductDimensions>
+          <ProductDimensions>
+              This product requires assembly.
+          </ProductDimensions>
+          <Designer>
+              <DimensionsHeadline>Designer:</DimensionsHeadline>
+              <ProductDimensions>ME I MADE THIS</ProductDimensions>
+          </Designer>
+        </ProductInfoRight>
+      </div>;
 }
 
 
