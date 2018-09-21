50a30bd745301e779ec9d11597e74170c0522842
Jeff Valle <jeffreyvalle@protonmail.com>
2018-09-20 19:45:36 -0700

    Tabs finished

--- client/components/tabs/assembly-documents/TaskRabbit.jsx
+++ client/components/tabs/assembly-documents/TaskRabbit.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 
-const Container = styled.div`
+const ZipCodeFinder = styled.div`
     width: 100%;
     height: 150px;
     clear: both;
-    text-align: center;
     margin-top: 45px;
     display:block;
     font-size: .6em;
@@ -33,25 +32,76 @@ const Headline = styled.div`
 const Subline = styled.div`
     font-size: 10px;
     font-weight: 200;
+    margin-left: 15px;
 `;
 
 const Logo = styled.img`
-    height: auto;
+    height: 37px;
+    width: 130px;
     margin-top: 9px;
     margin-left: 15px;
 `;
 
+const ZipCodeForm = styled.form`
+  text-align: center;
+`;
+
+const InputContainer = styled.form`
+  width: 180px;
+  display: table-cell;
+  margin-left: 0px;
+  float: left;
+  font-size: 9.6px !important;
+`;
+
+const ZipCodeInput = styled.input`
+  height: 26px;
+  margin-top: 10px;
+  width: 96px;
+  float: left;
+  margin-left: 15px;
+  padding-left: 10px !important;
+  padding-right: 10px !important;
+  border: 1px solid grey;
+  background-color: #fff;
+  -webkit-rtl-ordering: logical;
+  cursor: text;
+  -webkit-appearance: textfield;
+`;
+
+const ZipCodeButton = styled.input`
+  width: 30px;
+  height: 30px;
+  background-color: #0051ba;
+  margin-left: -23px;
+  position: absolute;
+  margin-top: 10px;
+  cursor: pointer;
+  border: none;
+  background-image: url(https://ww8.ikea.com/ext/iplugins/en_US/development/plugins/taskrabbit/images/button.png);
+  background-position: center;
+  background-size: 35px 35px;
+`;
+
+
+
+
 const TaskRabbit = () => {
-    return <Container>
-        <Logo src="./trlogo.svg"></Logo>
-        <HeadlineContainer>
-          <Headline>Looking to book an assembly service?</Headline>
-          <Subline>
-            Find out if an assembly service is available in your area
-          </Subline>
-        </HeadlineContainer>
-        TaskRabbit placeholder.
-      </Container>;
+    return (<ZipCodeFinder>
+            <Logo src="./trlogo.svg"></Logo>
+            <HeadlineContainer>
+                <Headline>Looking to book an assembly service?</Headline>
+                <Subline>
+                    Find out if an assembly service is available in your area
+                </Subline>
+            </HeadlineContainer>
+            <ZipCodeForm>
+                <InputContainer>
+                <ZipCodeInput placeholder="ZIP code"></ZipCodeInput>
+                <ZipCodeButton type='button'></ZipCodeButton>
+                </InputContainer>
+            </ZipCodeForm>
+        </ZipCodeFinder>);
 }
 
 export default TaskRabbit;
--- client/components/tabs/package-details/PackageDetailsBodyFrame.jsx
+++ client/components/tabs/package-details/PackageDetailsBodyFrame.jsx
@@ -3,17 +3,142 @@ import styled from 'styled-components';
 import ArticleNumber from './ArticleNumber.jsx';
 import Packages from './Packages.jsx';
 
+const Container = styled.div`
+  display: block;
+  padding-bottom: 1.04em;
+  overflow: hidden;
+  box-sizing: border-box;
+  padding: 20px 15px;
+  width: 1060px;
+  text-align: left;
+  font-size: 0.6em;
+`;
+
+const Pad = styled.div`
+  margin-bottom: 10px !important;
+  color: #333;
+  font-size: 13px;
+`;
+
+const PackageCount = styled.span`
+  font-size: 13px;
+  margin-bottom: 20px;
+  padding-right: 0.104em;
+  font-weight: bold;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  font-family: verdana !important;
+  font-weight: normal !important;
+  font-size: 13px !important;
+  border-collapse: collapse;
+  text-decoration: none;
+  color: #333;
+  display: table;
+  text-align: left;
+`;
+
+const Body = styled.tbody`
+  display: table-row-group;
+  vertical-align: middle;
+  border-color: inherit;
+  font-family: verdana !important;
+  font-weight: normal !important;
+  font-size: 13px !important;
+  border-collapse: collapse;
+  color: #333;
+  border-spacing: 2px;
+  text-align: left;
+`;
+
+const RowDark = styled.tr`
+  text-decoration: none;
+  color: #333;
+  font-family: Verdana, sans-serif;
+  line-height: 13px;
+  display: table-row;
+  vertical-align: inherit;
+  border-color: inherit;
+  font-weight: normal !important;
+  font-size: 13px !important;
+  border-collapse: collapse;
+  border-spacing: 2px;
+  background-color: #f4f4f4;
+`;
+
+const Row = styled.tr`
+  text-decoration: none;
+  color: #333;
+  font-family: Verdana, sans-serif;
+  line-height: 13px;
+  display: table-row;
+  vertical-align: inherit;
+  border-color: inherit;
+  font-weight: normal !important;
+  font-size: 13px !important;
+  border-collapse: collapse;
+  border-spacing: 2px;
+`;
+
+const Header = styled.th`
+  background-color: white;
+  text-align: left;
+  font-weight: bold;
+  display: table-cell;
+  vertical-align: inherit;
+  width: 80px;
+`;
+
+const IdCell = styled.td`
+  width: 10em;
+  padding-top: 4px;
+  padding-right: 0px;
+  padding-bottom: 4px;
+  padding-left: 0px;
+  display: table-cell;
+  vertical-align: inherit;
+`;
+
+const LinkCell = styled.td`
+  color: #3399fd;
+  text-decoration: underline;
+  cursor: pointer;
+  line-height: 13px;
+`;
+
 class PackageDetailsBodyFrame extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        return (<div>
-            <h1>Package Details Body Frame</h1>
-            <ArticleNumber />
-            <Packages />
-        </div>);
+        return <Container>
+            <Pad>
+              <PackageCount>Packages: </PackageCount>
+              &nbsp;1
+            </Pad>
+            <Table>
+              <Body>
+                <Row>
+                  <Header>Article number</Header>
+                  <Header>Packages</Header>
+                  <Header>Width</Header>
+                  <Header>Height</Header>
+                  <Header>Length</Header>
+                  <Header>Diameter</Header>
+                </Row>
+                <RowDark>
+                  <IdCell>40323437</IdCell>
+                  <IdCell>	1</IdCell>
+                  <IdCell>	16 ¼ "</IdCell>
+                  <IdCell>7 ½ "</IdCell>
+                  <IdCell>16 ¾ "</IdCell>
+                  <IdCell>-</IdCell>
+                </RowDark>
+              </Body>
+            </Table>
+          </Container>;
     }
 }
 
