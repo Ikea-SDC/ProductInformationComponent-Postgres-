40fc67ac9accb877b927843b5ac6f82e4be5ae17
Kevin Pozzi <kevindavidpozzi@gmail.com>
2018-09-28 20:17:20 -0700

    created file creating function for postgress

--- database/models.js
+++ database/models.js
@@ -3,38 +3,28 @@ require('./index.js')
 module.exports = function(sequelize, DataTypes) {
   var Product = sequelize.define('Product', {
     productId: DataTypes.INTEGER,
-    product_info: {
-      product_dimensions: {
-        max: DataTypes.INTEGER,
-        width: DataTypes.INTEGER,
-        diameter: DataTypes.INTEGER,
-        cord_length: DataTypes.INTEGER,
-        power: DataTypes.INTEGER,
-        assembly: DataTypes.BOOLEAN,
-        designer: DataTypes.STRING
-      },
-      bullets: DataTypes.ARRAY(DataTypes.STRING),
-      good_to_know: DataTypes.ARRAY(DataTypes.STRING),
-      care_instructions: DataTypes.ARRAY(DataTypes.STRING)
-    },
-    materials_env: {
-      weight: DataTypes.DataTypes.ARRAY(DataTypes.STRING),
-      shade: DataTypes.ARRAY(DataTypes.STRING),
-      tube: DataTypes.ARRAY(DataTypes.STRING)
-    },
-    assembly_docs: {
-      instructions: DataTypes.STRING,
-      manual: DataTypes.STRING
-    },
-    package_details: {
-      article_number: DataTypes.INTEGER,
-      packages: DataTypes.INTEGER,
-      width: DataTypes.INTEGER,
-      height: DataTypes.INTEGER,
-      length: DataTypes.INTEGER,
-      weight: DataTypes.INTEGER
-    }
-  }); 
+    prod_max: DataTypes.INTEGER,
+    prod_width: DataTypes.INTEGER,
+    prod_diameter: DataTypes.INTEGER,
+    prod_cord_length: DataTypes.INTEGER,
+    prod_power: DataTypes.INTEGER,
+    prod_assembly: DataTypes.BOOLEAN,
+    prod_designer: DataTypes.STRING,
+    bullets: DataTypes.ARRAY(DataTypes.STRING),
+    good_to_know: DataTypes.ARRAY(DataTypes.STRING),
+    care_instructions: DataTypes.ARRAY(DataTypes.STRING),
+    weight: DataTypes.DataTypes.ARRAY(DataTypes.STRING),
+    shade: DataTypes.ARRAY(DataTypes.STRING),
+    tube: DataTypes.ARRAY(DataTypes.STRING),
+    instructions: DataTypes.STRING,
+    manual: DataTypes.STRING,
+    article_number: DataTypes.INTEGER,
+    packages: DataTypes.INTEGER,
+    package_width: DataTypes.INTEGER,
+    package_height: DataTypes.INTEGER,
+    package_length: DataTypes.INTEGER,
+    package_weight: DataTypes.INTEGER
+  })
 
   const review = sequelize.define('review', {
     productId: DataTypes.INTEGER,
@@ -43,14 +33,13 @@ module.exports = function(sequelize, DataTypes) {
     review_title: DataTypes.STRING,
     created_at: DataTypes.STRING,
     recommended: DataTypes.BOOLEAN,
-    helpful: { yes: DataTypes.INTEGER, no: DataTypes.INTEGER },
-    specifics: {
-      value: DataTypes.INTEGER,
-      quality: DataTypes.INTEGER,
-      appearance: DataTypes.INTEGER,
-      ease: DataTypes.INTEGER,
-      expected: DataTypes.INTEGER
-    },
+    helpful_yes: DataTypes.INTEGER,
+    helpful_no: DataTypes.INTEGER,
+    value: DataTypes.INTEGER,
+    quality: DataTypes.INTEGER,
+    appearance: DataTypes.INTEGER,
+    ease: DataTypes.INTEGER,
+    expected: DataTypes.INTEGER,
     username: DataTypes.STRING
   })
 }
